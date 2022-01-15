4364048233eb4e63a0935c67067dcf140ebc856e
g-hamilton <greghamilton1@gmail.com>
2022-01-15 10:57:47 +0000

    banner component with font

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+import Banner from "../components/banner/banner";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,6 +13,14 @@ export default function Home() {
       </Head>
 
       <h1>Netflix</h1>
+
+      {/* <Navbar /> */}
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
+      {/* <Card /> */}
     </div>
   );
 }
