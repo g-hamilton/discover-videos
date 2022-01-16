e33d05de93b44f5d7a2bfdc84660b96bf52d8942
g-hamilton <greghamilton1@gmail.com>
2022-01-16 13:03:36 +0000

    nav bar links and dropdown

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,34 +1,76 @@
 import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 import styles from "./navbar.module.css";
 
 const Navbar = (props) => {
   const { username } = props;
 
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const router = useRouter();
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
+  const handleShowDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <Link href="/">
           <a className={styles.logoLink}>
-            <div className={styles.logoWrapper}>Logo</div>
+            <div className={styles.logoWrapper}>
+              <Image
+                alt="Netflix logo"
+                src={"/static/netflix.svg"}
+                width={128}
+                height={34}
+              />
+            </div>
           </a>
         </Link>
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>Somewhere else</li>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
         </ul>
         <nav className={styles.navContainer}>
           <div>
-            <button className={styles.usernameBtn}>
+            <button className={styles.usernameBtn} onClick={handleShowDropdown}>
               <p className={styles.username}>{username}</p>
-              {/* expand more icon */}
+              <Image
+                alt="Expand menu icon"
+                src={"/static/expand_more.svg"}
+                width={24}
+                height={24}
+              />
             </button>
-            <div className={styles.navDropdown}>
-              <div>
-                <a className={styles.linkName}>Sign out</a>
-                <div className={styles.lineWrapper}></div>
+            {showDropdown && (
+              <div className={styles.navDropdown}>
+                <div>
+                  <Link href="/login">
+                    <a className={styles.linkName}>Sign out</a>
+                  </Link>
+                  <div className={styles.lineWrapper}></div>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </nav>
       </div>
