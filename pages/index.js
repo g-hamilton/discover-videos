52fa510b07d70ff24a8a4ecc23cec799ac9d42f3
g-hamilton <greghamilton1@gmail.com>
2022-01-19 13:32:47 +0000

    fetching disney videos from youtube api

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import SectionCards from "../components/card/section-cards";
 import { getVideos } from "../lib/videos";
 
 export async function getServerSideProps() {
-  const disneyVideos = getVideos();
+  const disneyVideos = await getVideos();
 
   return { props: { disneyVideos } };
 }
