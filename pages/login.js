258586c7dfe748edcaf6543ceff3127f2c5c1748
g-hamilton <greghamilton1@gmail.com>
2022-01-20 12:28:18 +0000

    email validation

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,25 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [usrMsg, setUsrMsg] = useState("");
+
+  const handleOnChangeEmail = (e) => {
+    setUsrMsg("");
+    const value = e.target.value;
+    setEmail(value);
+  };
+
   const handleLoginWithEmail = (e) => {
     e.preventDefault();
-    console.log("hi login btn");
+    if (!email) {
+      setUsrMsg("Enter a valid email.");
+    }
   };
 
   return (
@@ -37,8 +49,9 @@ const Login = () => {
             type="email"
             placeholder="Email address"
             className={styles.emailInput}
+            onChange={handleOnChangeEmail}
           />
-          <p className={styles.usrMsg}></p>
+          <p className={styles.usrMsg}>{usrMsg}</p>
           <button className={styles.loginBtn} onClick={handleLoginWithEmail}>
             Go
           </button>
