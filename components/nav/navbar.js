fcab392b6ce5d44d2e525370796639da405fd926
g-hamilton <greghamilton1@gmail.com>
2022-01-21 14:49:45 +0000

    implemented signout

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -48,6 +48,19 @@ const Navbar = () => {
     setShowDropdown(!showDropdown);
   };
 
+  const handleSignout = async (e) => {
+    e.preventDefault();
+
+    try {
+      await magic.user.logout();
+      console.log(await magic.user.isLoggedIn()); // => 'false'
+      router.push("/login");
+    } catch (err) {
+      console.error("Error logging out of Magic:", err);
+      router.push("/login");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -85,9 +98,9 @@ const Navbar = () => {
             {showDropdown && (
               <div className={styles.navDropdown}>
                 <div>
-                  <Link href="/login">
-                    <a className={styles.linkName}>Sign out</a>
-                  </Link>
+                  <a className={styles.linkName} onClick={handleSignout}>
+                    Sign out
+                  </a>
                   <div className={styles.lineWrapper}></div>
                 </div>
               </div>
