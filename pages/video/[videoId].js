81c192e34fd5547aa480a8479b9fd61f7b54a5eb
g-hamilton <greghamilton1@gmail.com>
2022-01-23 11:32:23 +0000

    modal page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,10 +1,26 @@
 import { useRouter } from "next/router";
 
+// https://github.com/reactjs/react-modal#api-documentation
+import Modal from "react-modal";
+Modal.setAppElement("#__next");
+
+import styles from "../../styles/Video.module.css";
+
 const Video = () => {
   const router = useRouter();
   const { videoId } = router.query;
 
-  return <div>video page</div>;
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      ></Modal>
+    </div>
+  );
 };
 
 export default Video;
