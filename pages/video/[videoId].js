e70f2bdccb786f9949508e5dc59b7c7a62ae767f
g-hamilton <greghamilton1@gmail.com>
2022-01-23 19:49:37 +0000

    ISR video detail page data

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -5,24 +5,53 @@ import clsx from "classnames";
 import Modal from "react-modal";
 Modal.setAppElement("#__next");
 
+import Navbar from "../../components/nav/navbar";
+
 import styles from "../../styles/Video.module.css";
 
-const Video = () => {
+import { getYoutubeVideoById } from "../../lib/videos";
+
+/*
+  This page uses ISR.
+  https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration
+*/
+export async function getStaticProps(context) {
+  const { videoId } = context.params;
+  const videoArray = await getYoutubeVideoById(videoId);
+
+  return {
+    props: {
+      video: videoArray.length > 0 ? videoArray[0] : {},
+    },
+    revalidate: 10, // In seconds
+  };
+}
+
+export async function getStaticPaths() {
+  const staticVideos = ["mYfJxlgR2jw", "1VIZ89FEjYI", "4zH5iYM4wJo"];
+
+  const paths = staticVideos.map((videoId) => ({
+    params: { videoId },
+  }));
+
+  return { paths, fallback: "blocking" };
+}
+
+const Video = ({ video }) => {
   const router = useRouter();
   const { videoId } = router.query;
 
-  const video = {
-    title: "Hi cute dog",
-    publishTime: "2022-01-01",
-    description: "A big red dog that is super cute. Can he get any bigger?",
-    channelTitle: "Paramount Pictures",
-    viewCount: 1442440,
-  };
-
-  const { title, publishTime, description, channelTitle, viewCount } = video;
+  const {
+    title,
+    publishTime,
+    description,
+    channelTitle,
+    statistics: { viewCount } = { viewCount: 0 },
+  } = video;
 
   return (
     <div className={styles.container}>
+      <Navbar />
       <Modal
         isOpen={true}
         contentLabel="Watch the video"
@@ -48,7 +77,7 @@ const Video = () => {
             </div>
             <div className={styles.col2}>
               <p className={clsx(styles.subText, styles.subTextWrapper)}>
-                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.textColor}>Studio: </span>
                 <span className={styles.channelTitle}>{channelTitle}</span>
               </p>
               <p className={clsx(styles.subText, styles.subTextWrapper)}>
