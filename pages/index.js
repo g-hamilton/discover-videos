33ea10e249037789289d372a2fadc511cff9f720
g-hamilton <greghamilton1@gmail.com>
2025-02-17 15:22:14 +0000

    test push

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home({
   return (
     <div className={styles.container}>
       <Head>
-        <title>DV</title>
+        <title>DVs</title>
         <meta name='description' content='What would you like to watch next?' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
