992bbb89adcfc835efcf91240ba54ce8004af142
g-hamilton <greghamilton1@gmail.com>
2025-02-18 08:08:50 +0000

    test push

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home({
   return (
     <div className={styles.container}>
       <Head>
-        <title>Discover Videos App</title>
+        <title>Discover Videos</title>
         <meta name='description' content='What would you like to watch next?' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
