e2ff1666cbffcfedec696ce8590df3711f8bd632
g-hamilton <greghamilton1@gmail.com>
2025-02-20 09:43:00 +0000

    Temp remove middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
-import { verifyToken } from "../lib/utils";
+import { NextResponse } from 'next/server';
+import { verifyToken } from '../lib/utils';
 
 export async function middleware(req) {
   const token = req ? req.cookies?.token : null;
@@ -7,15 +7,15 @@ export async function middleware(req) {
 
   const { pathname } = req.nextUrl;
 
-  if (
-    pathname.includes("/api/login") ||
-    userId ||
-    pathname.includes("/static")
-  ) {
-    return NextResponse.next();
-  }
+  // if (
+  //   pathname.includes("/api/login") ||
+  //   userId ||
+  //   pathname.includes("/static")
+  // ) {
+  //   return NextResponse.next();
+  // }
 
-  if (!token && pathname !== "/login") {
-    return NextResponse.redirect("/login");
-  }
+  // if (!token && pathname !== "/login") {
+  //   return NextResponse.redirect("/login");
+  // }
 }
