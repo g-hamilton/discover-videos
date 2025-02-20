921d70278aa6606216b9cdce77f12d704be95aa7
g-hamilton <greghamilton1@gmail.com>
2025-02-20 09:49:22 +0000

    re-add middleware for test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -7,15 +7,15 @@ export async function middleware(req) {
 
   const { pathname } = req.nextUrl;
 
-  // if (
-  //   pathname.includes("/api/login") ||
-  //   userId ||
-  //   pathname.includes("/static")
-  // ) {
-  //   return NextResponse.next();
-  // }
+  if (
+    pathname.includes('/api/login') ||
+    userId ||
+    pathname.includes('/static')
+  ) {
+    return NextResponse.next();
+  }
 
-  // if (!token && pathname !== "/login") {
-  //   return NextResponse.redirect("/login");
-  // }
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect('/login');
+  }
 }
